ed4a174a933a3f4c433c6c5f5e7b0007e605f893
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 08:01:35 +0000

    did i get it right this time??

--- custom.js
+++ custom.js
@@ -1,4 +1,7 @@
-var links = document.getElementsByTagName("a");
-console.log(links);
-console.log(links.length);
-
+window.onload = function() {
+  var links = document.getElementsByTagName("a");
+  foreach(var i in links) {
+    var h = links[i].href;
+    links[i].href = h.replace(/\.md$/, "");
+  }
+};
