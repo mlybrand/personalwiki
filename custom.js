732dffa6ca540a433108871684c6cd5c083ac647
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 08:03:50 +0000

    getting close if I am not there yet

--- custom.js
+++ custom.js
@@ -2,6 +2,6 @@ window.onload = function() {
   var links = document.getElementsByTagName("a");
   for(var i in links) {
     var h = links[i].href;
-    links[i].href = h.replace(/\.md$/, "");
+    h.replace && links[i].href = h.replace(/\.md$/, "");
   }
 };
