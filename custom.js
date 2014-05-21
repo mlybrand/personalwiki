d8386a8d6089ddde39043badcae5889367d1a69d
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 08:06:26 +0000

    one more time

--- custom.js
+++ custom.js
@@ -2,6 +2,6 @@ window.onload = function() {
   var links = document.getElementsByTagName("a");
   for(var i in links) {
     var h = links[i].href;
-    if (h.replace) { links[i].href = h.replace(/\.md$/, ""); }
+    if (typeof h.replace === "function") { links[i].href = h.replace(/\.md$/, ""); }
   }
 };
