15f6255daebf7f80dcc8620a7c28c73273082a0d
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 07:47:18 +0000

    I will just keep doing this until i get it right

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
 var links = document.getElementsByTagName("a");
+console.log(links);
 for(var i in links) {
-  var h = links[i].href;
+  //var h = links[i].href;
   //links[i].href = h.replace(/\.md$/, "");
-  console.log(h);
+  //console.log(h);
 }
 
