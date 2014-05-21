43e9efdc869d4d252d01e6dbf5bcf6040e6ac439
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 07:48:59 +0000

    again...

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 var links = document.getElementsByTagName("a");
 console.log(links);
 for(var i in links) {
+  console.log(links[i]);
   //var h = links[i].href;
   //links[i].href = h.replace(/\.md$/, "");
   //console.log(h);
