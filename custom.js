75d1ba98fec5c150803fbdf1f69d3dc3823b7733
Mark Lybrand <mlybrand@gmail.com>
2014-05-21 08:07:30 +0000

    now?

--- custom.js
+++ custom.js
@@ -2,6 +2,6 @@ window.onload = function() {
   var links = document.getElementsByTagName("a");
   for(var i in links) {
     var h = links[i].href;
-    if (typeof h.replace === "function") { links[i].href = h.replace(/\.md$/, ""); }
+    if (h && h.replace) { links[i].href = h.replace(/\.md$/, ""); }
   }
 };
